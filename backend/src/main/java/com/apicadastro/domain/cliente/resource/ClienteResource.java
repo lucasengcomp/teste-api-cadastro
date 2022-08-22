bae4fd86930d5079439b2829ee073e08d1387dc2
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2022-08-22 10:55:06 -0300

    Busca paginada

--- backend/src/main/java/com/apicadastro/domain/cliente/resource/ClienteResource.java
+++ backend/src/main/java/com/apicadastro/domain/cliente/resource/ClienteResource.java
@@ -3,11 +3,11 @@ package com.apicadastro.domain.cliente.resource;
 import com.apicadastro.core.cliente.entity.dto.ClienteDTO;
 import com.apicadastro.domain.cliente.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/clientes")
@@ -20,5 +20,16 @@ public class ClienteResource {
     public ResponseEntity<ClienteDTO> findById(@PathVariable Long id) {
         ClienteDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClienteDTO>> findAllPaged(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "cfop") String orderBy) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<ClienteDTO> dtos = service.findAllPaged(pageRequest);
+        return ResponseEntity.ok(dtos);
     }
 }
--- backend/src/main/java/com/apicadastro/domain/cliente/service/ClienteService.java
+++ backend/src/main/java/com/apicadastro/domain/cliente/service/ClienteService.java
@@ -5,6 +5,8 @@ import com.apicadastro.core.exception.service.ResourceNotFoundException;
 import com.apicadastro.domain.cliente.entity.Cliente;
 import com.apicadastro.domain.cliente.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,5 +25,11 @@ public class ClienteService {
         Optional<Cliente> clientePorId = repository.findById(id);
         Cliente entity = clientePorId.orElseThrow(() -> new ResourceNotFoundException(RESOURCE_NOT_FOUND));
         return new ClienteDTO(entity, entity.getEnderecos());
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClienteDTO> findAllPaged(PageRequest pageRequest) {
+        Page<Cliente> clientes = repository.findAll(pageRequest);
+        return clientes.map(x -> new ClienteDTO(x, x.getEnderecos()));
     }
 }
