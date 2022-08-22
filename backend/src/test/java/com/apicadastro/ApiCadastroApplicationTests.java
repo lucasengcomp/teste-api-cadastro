999ae353f8a348beb402e33987b6c7e6cf77bedb
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2022-08-21 22:20:17 -0300

    Projeto criado

---
+++ backend/src/test/java/com/apicadastro/ApiCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apicadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
